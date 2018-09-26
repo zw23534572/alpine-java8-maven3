2b04dd001cbab2dab6934c43e6fb244217ae9a75
manson.zhou <manson.zhou@dzong.com>
2018-09-26 18:07:39 +0800

    写好注释

--- deploy.sh
+++ deploy.sh
@@ -1,41 +1,43 @@
-project_name="xx"
-out_put_path="/export/output/"
-git_path=$1
-git_branch=$2
-sub_project_path=$3
-current_path="/export/App"
+project_name="" #项目名称
+current_path="/export/App" #源码路径
+out_put_path="/export/output/" #将编译好的class文件与service.sh放到该文件下,映射出去
+git_path=$1	#需要编译的源码git地址
+git_branch=$2 #需要编译的源码分支
+sub_project_path=$3 #需要编译的源码子目录路径
 
+
+# 获取当前的项目名称
 get_project_name() {
 	project_name=${git_path##*/}
 	project_name=${project_name%.*}
 	echo "项目名称为 $project_name"
 }
 
+# 开始验证
 validation() {
-	# 入参1：代表项目名称
 	if [ ! -n "$git_path" ]; then
 		echo "请填写提交git地址"
 		exit 1
 	fi
 }
 
+# 切换分支
 switch_branch() {
-	# 入参1：代表项目名称
 	if [ -n "$git_branch" ]; then
 		git checkout -b $git_branch origin/$git_branch
 	fi
 }
 
+# 主 main 函数
 main() {
-	echo "入参git_path:$git_path,入参git_branch:$git_branch,入参3:$sub_project_path"
+	echo "获取所有入参: 入参git_path:$git_path,入参git_branch:$git_branch,入参sub_project_path:$sub_project_path"
 	validation
 
+	echo "工作路径设置到到源码区 $current_path"
 	mkdir -p current_path
-
 	cd $current_path
 
-	get_project_name $git_path
-
+	echo "如果源码区未有该项目[$project_name]，则使用git clone" 
 	if [ ! -d "$project_name" ]; then
 		echo "git clone $git_path"
 		git clone $git_path
@@ -43,7 +45,8 @@ main() {
 		cd $project_name
 		echo "switch_branch"
 		switch_branch
-	else
+	else 
+		echo "如果源码区存在该项目[$project_name]，则使用git pull" 
 		echo "cd $project_name"
 		cd $project_name
 		echo "switch_branch"
@@ -52,20 +55,24 @@ main() {
 		git pull
 	fi
 
-    
+	echo "如果git地址需要编译的源码在子目录，切换到子目录[$sub_project_path]" 
 	if [ -n "$sub_project_path" ]; then
 		echo "cd $sub_project_path"
 		cd $sub_project_path
 	fi
 
+	echo "如果当前路径没有pom文件，无法进行pom编译" 
 	if [ -f "pom.xml" ]; then
 		echo "mvn package"
 		mvn package
-		echo "cp target/*.jar $out_put_path"
-		cp target/*.jar $out_put_path
 	fi
 
+	echo "将编译好的源码以及service.sh放到[$out_put_path]"
 	cp service.sh $out_put_path
+	mkdir $out_put_path/$project_name
+	cp target/*.jar $out_put_path/$project_name
 }
 
+# 获取项目名称
+get_project_name $git_path
 main
